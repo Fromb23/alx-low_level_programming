a992fa46c9825e950dd1a3da1aec3b56e803a477
Fromb23 <rombo.programming@gmail.com>
2023-09-07 11:33:40 -0700

    code created with Betty Styling

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message to the console.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
