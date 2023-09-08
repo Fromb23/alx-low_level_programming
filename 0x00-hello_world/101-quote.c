a774145c92c2d140b50e7217c13ba89a8480c67f
Fromb23 <rombo.programming@gmail.com>
2023-09-07 19:12:27 -0700

    101-quote.c file syntax corrected with Betty style

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the specified message to the standard error stream.
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, \
+
+	2015-10-19\n";
+
+	write(STDERR_FILENO, message, 59);
+
+	return (1);
+}
