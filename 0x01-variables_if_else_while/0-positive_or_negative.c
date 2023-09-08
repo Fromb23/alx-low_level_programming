204f68b5b98ab61d70720c2958d94f0a1f7d8686
Fromb23 <rombo.programming@gmail.com>
2023-09-08 11:33:04 -0700

    compilation error corrected and the code is working as fine

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,20 +18,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("Generated random number: %d\n", n);
-
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
 	}
-	else if (n < 0)
-	{
-	printf("%d is negative\n", n);
-	}
-	else
+	else if (n == 0)
 	{
 	printf("%d is zero\n", n);
 	}
+	else
+	{
+	printf("%d is negative\n", n);
+	}
+	printf("\n");
 
 	return (0);
 }
