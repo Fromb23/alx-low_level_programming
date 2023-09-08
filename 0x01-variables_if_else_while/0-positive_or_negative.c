c2c4343de7e9af678fdd78e7ddbb2c6aae244c01
Fromb23 <rombo.programming@gmail.com>
2023-09-08 13:17:29 -0700

    syntax error corrected

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,17 +20,17 @@ int main(void)
 
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+	printf("%d is positive", n);
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+	printf("%d is zero", n);
 	}
 	else
 	{
-	printf("%d is negative\n", n);
+	printf("%d is negative", n);
 	}
-	printf("\n")
+	printf("\n");
 
 	return (0);
 }
