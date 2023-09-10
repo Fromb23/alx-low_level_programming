0da04a4ccd017a093a0bfb8a39ecb259452cdd4e
Fromb23 <rombo.programming@gmail.com>
2023-09-09 23:01:30 -0700

    prints alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - This is the main Function of the program
+*
+* Descripition - This code print alphabetic letters in reverse
+*
+* return: Always 0 (SUCCESS)
+**/
+
+int main(void)
+
+{
+	char alphabet = 'z';
+
+	do
+
+	{
+	putchar(alphabet);
+	alphabet--;
+	}
+
+	while
+
+	(alphabet >= 'a');
+
+	putchar('\n');
+
+	return (0);
+}
