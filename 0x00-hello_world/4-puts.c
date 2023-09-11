21cae5acbfc05aae85143687096339f775656bb3
Fromb23 <rombo.programming@gmail.com>
2023-09-10 23:55:01 -0700

    size32 and size64 code generated, syntax error corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: This function prints a message to the console.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
