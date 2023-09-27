8937543a349fda73f9c27ec23105073909a0ef35
Fromb23 <rombo.programming@gmail.com>
2023-09-27 04:21:56 -0700

    recursion added

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,11 +12,13 @@ void _puts_recursion(char *s)
 
 {
 
-	while (*s != '\0')
+	if (*s != '\0')
 	{
-
-	_putchar(*s);
-	s++;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+	else
+	{
 	_putchar('\n');
+	}
 }
