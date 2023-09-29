ef7af28eeb070822b2f82201d4ebf0f2f3224cdd
Fromb23 <rombo.programming@gmail.com>
2023-09-29 15:25:00 -0700

    script created successfully

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-liball.a
+# This script compiles all .c files in the current directory into object files,
+# creates a static library named liball.a, and cleans up the
+# temporary object files.
+
+	for source_file in *.c; do
+	object_file="${source_file%.c}.o"
+	gcc -c "$source_file" -o "$object_file"
+done
+
+# Compile all .c files in the current directory into object files
+
+ar rcs liball.a *.o
+
+# Clean up: remove the temporary object files
+
+rm *.o
