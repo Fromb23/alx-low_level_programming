bffe699c55e8608cf92ed7750c5b6cf5a8234a61
Fromb23 <rombo.programming@gmail.com>
2023-10-02 10:21:27 -0700

    The program prints number of argumetns

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments (unused)
+ * @argv: An array of strings containing the command-line arguments (unused)
+ *
+ * Return: Always 0 (success)
+ **/
+
+int main(int argc, char *argv[])
+
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
