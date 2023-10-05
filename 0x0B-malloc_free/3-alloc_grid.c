568d515dd9cd66ed35e4b8229e7bb37626149c80
Fromb23 <rombo.programming@gmail.com>
2023-10-04 19:58:15 -0700

    2d achieved

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,11 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * alloc_grid - Allocates memory for a 2D grid of integers
+ * @width: The width (number of columns) of the grid
+ * @height: The height (number of rows) of the grid
+ *
+ * Return: A pointer to the allocated grid, or NULL on failure
+ **/
+
 int **alloc_grid(int width, int height)
 
 {
-	if (width == 0 || height == 0)
+	int i, j;
+	int **array;
+
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
+	array = (int **)malloc(height * sizeof(int *));
 
+	if (array == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+	array[i] = (int *)malloc(width * sizeof(int));
+	if (array[i] == NULL)
+	{
+		for (j = 0; j < i; j++)
+		{
+		free(array[j]);
+		}
+		free(array);
+	return (NULL);
+	}
+	for (j = 0; j < width; j++)
+	{
+		array[i][j] = 0;
+		}
+	}
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void free_grid(int **grid, int height)
+
+{
+
+	
