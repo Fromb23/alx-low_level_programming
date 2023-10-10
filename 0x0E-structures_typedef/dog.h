dae1818e1b8be3ca5405dcaac7ef0959719dce0c
Fromb23 <rombo.programming@gmail.com>
2023-10-10 09:17:46 -0700

    prototype comment removed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,13 +18,6 @@ struct dog
 	char *owner;
 };
 
-/**
- * init_dog - Initialize a struct dog
- * @d: Pointer to struct dog
- * @name: Pointer to dog's name string
- * @age: Age of the dog
- * @owner: Pointer to owner's name string
- **/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
