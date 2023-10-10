d6d097ddfd4ece0267616162bf2f32766f720b47
Fromb23 <rombo.programming@gmail.com>
2023-10-10 09:16:22 -0700

    comment added to the prototyp

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - Initialize a struct dog
+ * @d: Pointer to struct dog
+ * @name: Pointer to dog's name string
+ * @age: Age of the dog
+ * @owner: Pointer to owner's name string
+ **/
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* DOG_H */
