2160f4223c19c64b2d98af067d125ed1abd9853e
Fromb23 <rombo.programming@gmail.com>
2023-10-11 10:21:13 -0700

    nested function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stddef.h>
 #include "function_pointers.h"
 
@@ -11,4 +12,15 @@ void print_name(char *name, void (*f)(char *))
 
 {
 	(*f)(name);
+}
+
+/**
+ * print_name_f - Function to print a name.
+ * @name: The name to be printed.
+ **/
+
+void print_name_f(char *name)
+
+{
+	printf("%s\n", name);
 }
