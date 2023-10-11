6219b605444679052512a2f9d354a841d84d6309
Fromb23 <rombo.programming@gmail.com>
2023-10-10 23:00:15 -0700

    free grid function working

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,8 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - Frees a 2D grid previously created by `alloc_grid`.
+ * @grid: The 2D grid to be freed.
+ * @height: The height of the grid.
+ **/
+
 void free_grid(int **grid, int height)
 
 {
+	int i;
 
-	
+	if (grid == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
