0009ad5131ddda30c96158bf99de6b703d05758e
Fromb23 <rombo.programming@gmail.com>
2023-10-25 00:22:58 -0700

    recursive implemented instead of loop

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,12 @@
 void _puts_recursion(char *s)
 
 {
-	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+	else
 	_putchar('\n');
 }
