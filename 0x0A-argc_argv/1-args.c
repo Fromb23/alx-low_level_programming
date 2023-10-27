4a7619c244e919bcb0dd70ec6b9ff34fe631c9b2
Fromb23 <rombo.programming@gmail.com>
2023-10-27 02:23:39 -0700

    betty error corrected

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 on success.
+ **/
+
 int main(int argc, char *argv[])
 
 {
