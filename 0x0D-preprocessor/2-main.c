702afb816f09383f2a4180ab19e54be99d0ee7ec
Fromb23 <rombo.programming@gmail.com>
2023-11-06 09:15:39 -0800

    a program that prints its source file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ **/
+
+int main(int argc, char *argv[])
+
+{
+	(void)argc;
+	(void)argv;
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
