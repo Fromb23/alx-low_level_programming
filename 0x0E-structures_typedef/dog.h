2908ef020af5815c900e9e0e58e04a2e627617e7
Fromb23 <rombo.programming@gmail.com>
2023-11-06 18:08:08 -0800

    betty style applied in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Represents a dog with name, age, and owner information
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ **/
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+
 
 #endif
