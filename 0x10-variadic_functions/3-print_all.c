2f78489d6d227425b7994f6ffc86e0727544f303
Fromb23 <rombo.programming@gmail.com>
2023-11-09 11:44:36 -0800

    a simple prinf function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - Print values based on a format string.
+ * @format: The format string containing format specifiers.
+ * @...: Variable number of arguments based on the format string.
+ */
+
+void print_all(const char * const format, ...)
+
+{
+	va_list output;
+	const char *ptr;
+	int int_output;
+	double float_output, temp;
+	char char_output, *string_output;
+
+	va_start(output, format);
+	ptr = format;
+	while (ptr != NULL && *ptr != '\0')
+	{
+		switch (*ptr)
+		{
+			case 'c':
+				char_output = va_arg(output, int);
+				printf("%c", char_output);
+				break;
+			case 'i':
+				int_output = va_arg(output, int);
+				printf("%i", int_output);
+				break;
+			case 'f':
+				temp = va_arg(output, double);
+				float_output = (float) temp;
+				printf("%f", float_output);
+				break;
+			case 's':
+				string_output = va_arg(output, char *);
+				if (string_output == NULL)
+					printf("(nil)");
+				else
+					printf("%s", string_output);
+				break;
+			default:
+			break;
+		}
+		if (*(++ptr))
+			printf(", ");
+	}
+	printf("\n");
+	va_end(output);
+}
