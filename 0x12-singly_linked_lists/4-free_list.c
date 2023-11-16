75c29c343acd81745be2e902e40f62d8dab9bfb8
Fromb23 <rombo.programming@gmail.com>
2023-11-16 13:38:37 -0800

    free singled list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Free a linked list
+ * @head : a pointer to the head of the list
+ *
+ **/
+
+void free_list(list_t *head)
+
+{
+	list_t *node_temp;
+
+	while (head != NULL)
+	{
+		node_temp = head;
+		head = head->next;
+		free(node_temp->str);
+		free(node_temp);
+	}
+}
