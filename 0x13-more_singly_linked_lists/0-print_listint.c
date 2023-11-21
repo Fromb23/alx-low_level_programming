420b986f98e53d83c39f4c40c7cb47c568a74f6a
Fromb23 <rombo.programming@gmail.com>
2023-11-21 13:10:53 -0800

    betty error corrected

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,7 @@
  * print_listint - Prints all elements of a listint_t list
  * @h: Pointer to the head of the list
  *
- * Return: Number of nodes in the list
+ * Return: Number of nodes in the list if success and -1 if unsuccesful
  **/
 
 size_t print_listint(const listint_t *h)
