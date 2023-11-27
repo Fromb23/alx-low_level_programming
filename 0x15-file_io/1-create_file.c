823c49b9e2a331d34ab83dafabf92f5892af1cf1
Fromb23 <rombo.programming@gmail.com>
2023-11-27 09:56:52 -0800

    a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <sys/stat.h>
+
+/**
+ * create_file - Creates a file with the specified content.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to the file.
+ *
+ * Return: On success, 1. On failure, -1.
+ **/
+
+int create_file(const char *filename, char *text_content)
+
+{
+	FILE *file;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = fopen(filename, "w");
+	if (file == NULL)
+	{
+		perror("Error opening file");
+		return (-1);
+	}
+	if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
+	{
+		perror("Error setting file permissions");
+		fclose(file);
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		if (fputs(text_content, file) == EOF)
+		{
+			perror("Error writing to file");
+			fclose(file);
+			return (-1);
+		}
+	}
+	else
+	{
+		if (fputs("", file) == EOF)
+		{
+			perror("Error writing to file");
+			fclose(file);
+			return (-1);
+		}
+	}
+	fclose(file);
+
+	return (1);
+}
