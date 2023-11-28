e9f0f3bc47485b2b1e5f828c8737e82a4d9b35bb
Fromb23 <rombo.programming@gmail.com>
2023-11-27 20:52:19 -0800

    whitespace error corrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	file = fopen(filename, "w");
 	if (file == NULL)
 		return (-1);
-	
+
 	if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
 	{
 		fclose(file);
