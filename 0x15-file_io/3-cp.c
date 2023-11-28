ca5f03373f7aa9c7443380335abdc5bd8c19d4b6
Fromb23 <rombo.programming@gmail.com>
2023-11-27 21:16:22 -0800

    betty style rectified

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,65 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 #define BUFFER_SIZE 1024
 
-int main(int argc, char *argv[])
+/**
+ * handle_error - Print an error message, including the provided message and file path,
+ * and exit with a predefined error code.
+ *
+ * @message: The error message.
+ * @file_path: The file path associated with the error.
+ **/
+
+void handle_error(const char *message, const char *file_path)
+
+{
+	perror("Error: ");
+	dprintf(2, "%s%s\n", message, file_path);
+	exit(99);
+}
+
+/**
+ * copy_file - Copy the contents of one file to another and set permissions.
+ *
+ * @file_from: The source file path.
+ * @file_to: The destination file path.
+ **/
+
+void copy_file(const char *file_from, const char *file_to)
 
 {
 	FILE *file, *file_dest;
 	char buffer[BUFFER_SIZE];
 	size_t bytesRead;
-	const char *file_from, *file_to;
 	struct stat st;
 
-	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit (97);
-	}
-	file_from = argv[1];
 	file = fopen(file_from, "r");
 	if (file == NULL)
-	{
-		perror("Error: ");
-		dprintf(2,"Can't read from file%s\n", file_from);
-		exit (98);
-	}
-	
-	file_to = argv[2];
+	        handle_error("Can't read from file ", file_from);
+
 	file_dest = fopen(file_to, "w");
+
 	if (file_dest == NULL)
 	{
-		perror("Error: ");
-		dprintf(2, "Can't write to%s\n", file_to);
 		fclose(file);
-		exit (99);
+		handle_error("Can't write to ", file_to);
 	}
+
 	while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0)
 	{
 		if (fwrite(buffer, 1, bytesRead, file_dest) != bytesRead)
 		{
-			perror("Error: ");
-			dprintf(2, "Could not write to file '%s'\n", file_to);
 			fclose(file);
 			fclose(file_dest);
-			exit(100);
+			handle_error("Could not write to file '", file_to);
 		}
 	}
+
 	fclose(file);
-	if (fclose(file_dest) != 0)
+
+    	if (fclose(file_dest) != 0)
 	{
 		dprintf(2, "Error: Can't close fd FD_VALUE\n");
 		exit(100);
 	}
+
 	if (stat(file_to, &st) == -1)
 	{
 		dprintf(2, "Error: Could not get information about %s\n", file_to);
-		exit (101);
+		exit(101);
 	}
 	else
 	{
@@ -69,5 +78,32 @@ int main(int argc, char *argv[])
 			exit(102);
 		}
 	}
+}
+
+/**
+ * main - Entry point for the file copy program.
+ *
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: 0 on success, other values on failure.
+ **/
+
+int main(int argc, char *argv[])
+
+{
+	const char *file_from, *file_to;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	copy_file(file_from, file_to);
+
 	return (0);
 }
