0e9b5b3e071eb52eb0d200b6daa240f449ec635d
Fromb23 <rombo.programming@gmail.com>
2024-02-02 07:58:51 -0800

    hash table created

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
-		free(new_table);
 		return (NULL);
 	}
 
