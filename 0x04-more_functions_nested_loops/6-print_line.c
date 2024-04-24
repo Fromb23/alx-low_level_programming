cea50a1fe2de3de61592c7c705165fc0b4c2aedf
Fromb23 <rombo.programming@gmail.com>
2024-04-24 18:22:38 +0300

    new line corrected

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,13 +14,9 @@ void print_line(int n)
 	if (n > 0)
 	{
 		for (i = 1; i <= n; i++)
-		{
-		_putchar('_');
-		}
+			_putchar('_');
+	_putchar('\n');
 	}
 	else if (n <= 0)
-		{
 		_putchar('\n');
-		}
-	_putchar('\n');
 }
